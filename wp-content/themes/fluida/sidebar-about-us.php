4d3b0fb4eec707a68a4f4a16c6ad52a73c5dd25a
Andrew Smith <smitha110@winthrop.edu>
2017-02-22 18:45:21 +0000

    Added edited images to Wordpress, updated gallery page with new images and test video, began work on sidebar custom sidebars and templates, adjusted color scheme and header.

---
+++ wp-content/themes/fluida/sidebar-about-us.php
@@ -0,0 +1,33 @@
+<?php /* Template Name: AboutUs */ ?>
+
+
+
+<?php get_header(); ?>
+<style type="text/css">
+	div.about{
+   			 float: right;
+   			 max-width: 500px;
+   			 margin: 0;
+   			 padding: 0em;
+				}
+
+</style>
+
+		<div id="container" class="<?php echo fluida_get_layout_class(); ?>">
+
+		<main id="main" role="main" class="main">
+			<?php cryout_before_content_hook(); ?>
+			
+			<?php get_template_part( 'content/content', 'page' ); ?>
+
+			<?php cryout_after_content_hook(); ?>
+		</main><!-- #main -->
+		
+		<?php fluida_get_sidebar(); ?>
+
+	</div><!-- #container -->
+	
+	
+
+
+<?php get_footer(); ?>
